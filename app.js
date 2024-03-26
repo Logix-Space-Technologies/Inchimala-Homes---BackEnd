80d5683e5899bf10dc5ab6b0090649aa506b4484
lakshmiibalu <lakshmibalu74@gmail.com>
2024-03-26 13:06:33 +0530

    userlogin branch

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require("express")
 const cors = require("cors")
 const mysql = require("mysql")
+
 const caretakerrouter=require("./controllers/CaretakerRouter")
 const foodrouter=require("./controllers/foodRoute")
-
 const userRoute=require("./controllers/userRoute")
-
 const packagerouter=require("./controllers/packageRoute")
 
 
 const app = express()
 
 app.use(express.json())
+
 
 app.use("/api/caretaker",caretakerrouter)
 app.use("/api/food",foodrouter)
