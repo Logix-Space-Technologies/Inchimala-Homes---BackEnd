e467acb3f52b5eba348260ddc6437ea3b23371c6
Naslanas <naslava00@gmail.com>
2024-03-26 09:58:31 +0530

    env added

--- models/caretakerModel.js
+++ models/caretakerModel.js
@@ -1,12 +1,13 @@
 const mysql = require("mysql")
+require("dotenv").config()
 
 //MySQL connection
 
 const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'inchimala_db'
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    database:process.env.DB_NAME,
+    port:process.env.DB_PORT
 })
 
 const caretakerModel={
@@ -14,6 +15,7 @@ const caretakerModel={
         const query='INSERT INTO caretaker SET ?';
         pool.query(query,caretakerData,callback)
     }
+    
 }
 
 module.exports=caretakerModel
