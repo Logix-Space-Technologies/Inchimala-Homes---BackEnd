125ce7a5a2d28cf2222e0c4c4c701ad56ae927cf
ar7unms <msarjun077@gmail.com>
2024-03-27 09:59:31 +0530

    delete food

--- models/foodModel.js
+++ models/foodModel.js
@@ -13,7 +13,12 @@ const foodModel={
     insertfood:(foodData,callback)=>{
         const query='INSERT INTO food SET ?';
         pool.query(query,foodData,callback)
+    },
+    deletefood:(foodid,callback)=>{
+        const query='DELETE FROM food WHERE foodid = ?';
+        pool.query(query,foodid,callback)
     }
 }
+
 
 module.exports=foodModel
