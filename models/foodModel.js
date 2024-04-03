cf87208ee9c20ae31446310031d39e90bb552307
vidhya-c-v <vidhyacv14@gmail.com>
2024-04-03 12:06:31 +0530

    viewfoodbooking completed

--- models/foodModel.js
+++ models/foodModel.js
@@ -54,7 +54,12 @@ const foodModel={
     viewFood: (callback) => {
         const query = 'SELECT * FROM food';
         pool.query(query, callback);
-    }
+    },
+    
+    viewFoodBooking: (callback) => {
+        const query = 'SELECT * FROM foodbooking';
+        pool.query(query, callback);
+    } 
 
 
 
