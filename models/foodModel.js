ff05656aace0192459d0f6574e80e1f8abd783aa
krishnadam206 <krishnadasm206@gmail.com>
2024-04-04 14:02:17 +0530

    Committed

--- models/foodModel.js
+++ models/foodModel.js
@@ -49,27 +49,25 @@ const foodModel={
             return callback(null, results[0]);
         });
     },
-
-
-//FOOD BOOKING BY USER
-
+    getUserDetails: (userid, callback) => {
+        const query = 'SELECT * FROM user WHERE userid = ?';
+        pool.query(query, [userid], (error, results) => {
+            if (error) {
+                return callback(error, null);
+            }
+            if (results.length === 0) {
+                return callback(null, null); // No user found with the given ID
+            }
+            return callback(null, results[0]); // Return the first user found
+        });
+      },
+      
     bookFood: (bookingData, callback) => {
         const query = 'INSERT INTO foodbooking SET ?';
         pool.query(query, bookingData, callback);
     },
   
-    getUserDetails: (userid, callback) => {
-      const query = 'SELECT * FROM user WHERE userid = ?';
-      pool.query(query, [userid], (error, results) => {
-          if (error) {
-              return callback(error, null);
-          }
-          if (results.length === 0) {
-              return callback(null, null); // No user found with the given ID
-          }
-          return callback(null, results[0]); // Return the first user found
-      });
-    },
+    
 
     viewFood: (callback) => {
         const query = 'SELECT * FROM food';
