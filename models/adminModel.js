506093e1e9a430471a9667dc4b6c6a4074a807e0
Naslanas <naslava00@gmail.com>
2024-04-05 12:10:39 +0530

    admin token generation completed

--- models/adminModel.js
+++ models/adminModel.js
@@ -13,7 +13,19 @@ const adminModel={
     insertadmin:(adminData,callback)=>{
         const query='INSERT INTO admin SET ?';
         pool.query(query,adminData,callback)
-    }
+    },
+    loginAdmin: (emailid, callback) => {
+        const query = 'SELECT * FROM admin WHERE emailid = ? LIMIT 1'; 
+        pool.query(query, [emailid], (error, results) => {
+          if (error) {
+            return callback(error, null);
+          }
+          if (results.length === 0) {
+            return callback(null, null);
+          }
+          return callback(null, results[0]);
+        });
+    },
    
 }
 
