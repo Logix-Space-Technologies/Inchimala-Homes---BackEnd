f01b671e94f2c461ae12d2dc65ea875c7e2078af
amaldevk <amalvasudev111@gmail.com>
2024-04-30 09:58:48 +0530

    created viewcaretaker

--- models/caretakerModel.js
+++ models/caretakerModel.js
@@ -1,42 +1,42 @@
-const mysql = require("mysql")
-require("dotenv").config()
-//MySQL connection
-const pool = mysql.createPool({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:'',
-    database:process.env.DB_NAME,
-    port:process.env.DB_PORT
-})
+    const mysql = require("mysql")
+    require("dotenv").config()
+    //MySQL connection
+    const pool = mysql.createPool({
+        host:process.env.DB_HOST,
+        user:process.env.DB_USER,
+        password:'',
+        database:process.env.DB_NAME,
+        port:process.env.DB_PORT
+    })
 
-const caretakerModel={
-    insertCaretaker:(caretakerData,callback)=>{
-        const query='INSERT INTO caretaker SET ?';
-        pool.query(query,caretakerData,callback)
-    },
-   
-    loginCaretaker:(emailid, callback) => {
-        const query = 'SELECT * FROM caretaker WHERE emailid = ? LIMIT 1';
-        pool.query(query,[emailid],(error,results) => {
-            if (error) {
-                return callback(error, null);
-              }
-            if (results.length === 0) {
-                return callback(null, null);
-              }
-              return callback(null, results[0]);
-        });
-    },
-    deletecaretaker:(caretakerid,callback)=>{
-        const query = 'UPDATE caretaker SET deleteFlag = 1 WHERE caretakerid = ?';
-        pool.query(query,caretakerid,callback)
-    },
-    updateCaretaker: (caretakerId, updatedData, callback) => {
-    const query = 'UPDATE caretaker SET ? WHERE caretakerid = ?';
-    pool.query(query, [updatedData, caretakerId], callback);
-    }
+    const caretakerModel={
+        insertCaretaker:(caretakerData,callback)=>{
+            const query='INSERT INTO caretaker SET ?';
+            pool.query(query,caretakerData,callback)
+        },
     
-}
+        loginCaretaker:(emailid, callback) => {
+            const query = 'SELECT * FROM caretaker WHERE emailid = ? LIMIT 1';
+            pool.query(query,[emailid],(error,results) => {
+                if (error) {
+                    return callback(error, null);
+                }
+                if (results.length === 0) {
+                    return callback(null, null);
+                }
+                return callback(null, results[0]);
+            });
+        },
+        deletecaretaker:(caretakerid,callback)=>{
+            const query = 'UPDATE caretaker SET deleteFlag = 1 WHERE caretakerid = ?';
+            pool.query(query,caretakerid,callback)
+        },
+        updateCaretaker: (caretakerId, updatedData, callback) => {
+        const query = 'UPDATE caretaker SET ? WHERE caretakerid = ?';
+        pool.query(query, [updatedData, caretakerId], callback);
+        }
+        
+    }
 
-module.exports=caretakerModel
+    module.exports=caretakerModel
 
