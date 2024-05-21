2e0a2f8ce39a3e594a5accf48d9e16b2483dd97a
lakshmiibalu <lakshmibalu74@gmail.com>
2024-05-21 13:23:19 +0530

    booking

--- controllers/bookingRouter.js
+++ controllers/bookingRouter.js
@@ -46,14 +46,14 @@ router.post('/rejectBooking', (req, res) => {
 
     bookingModel.rejectBooking(bookingid, (error, results) => {
         if (error) {
-            res.status(500).send('Error retrieving  data');
+            res.json({status:'Error retrieving  data'})
             return;
         }
         if (results.length > 0) {
-            res.status(200).json(results[0]);
+            res.json({status:results[0]})
         }
         else {
-            res.status(404).send(`Booking rejected with ID : ${bookingid}`);
+            res.json({status:`Booking rejected with ID : ${bookingid}`})
         }
 
     });
