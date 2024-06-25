861bb41a8a85c7d62d73acace619c18c5d7ab705
krishnadam206 <krishnadasm206@gmail.com>
2024-06-25 13:46:18 +0530

    Committed

--- controllers/foodRoute.js
+++ controllers/foodRoute.js
@@ -183,35 +183,6 @@ router.get('/viewFoodBooking', (req, res) => {
     })
 });
 
-// //Reject Food Booking
-// router.post('/rejectFoodBooking', (req, res) => {
-//     const token = req.headers["token"]
-//     jwt.verify(token, "inchimalaCaretakerLogin", async (error, decoded) => {
-//         if (decoded && decoded.email) {
-
-//             var bookingid = req.body.bookingid
-//             foodModel.rejectFoodBooking(bookingid, (error, results) => {
-//                 if (error) {
-//                     res.status(500).send('Error retrieving  data');
-//                     return;
-//                 }
-//                 if (results.length > 0) {
-//                     res.status(200).json(results[0]);
-//                 }
-//                 else {
-//                     res.status(404).send(`Booking rejected with ID : ${bookingid}`);
-//                 }
-//             });
-//         }
-//         else {
-
-//             res.json(
-//                 { status: "unauthorized user" }
-//             )
-
-//         }
-//     })
-// });
 
 // Reject Food Booking
 router.post('/rejectFoodBooking', (req, res) => {
